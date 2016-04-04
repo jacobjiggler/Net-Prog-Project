7a2e14c72526aba55d8f3c26bfd901379095095b
jacobjiggler <jmart.alb@gmail.com>
2016-04-04 16:37:54 -0400

    added arp code

--- project1.py
+++ project1.py
@@ -3,4 +3,11 @@ import sys
 if __name__ == '__main__':
 	with open('routes.txt') as r:
 		content = r.readlines()
-	print content
+	print(content)
+
+
+	dict = {}
+	with open('arp.txt') as arp:
+		for line in arp:
+			dict[line.split(" ")[0]] = line.split(" ")[1]
+			print dict[line.split(" ")[0]]
