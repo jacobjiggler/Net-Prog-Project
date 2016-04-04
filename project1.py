52687181b5df0e4a443e38f42a7ad42f82b544a5
jacobjiggler <jmart.alb@gmail.com>
2016-04-04 18:10:25 -0400

    more basic logic

--- project1.py
+++ project1.py
@@ -9,7 +9,7 @@ class Node(object):
 if __name__ == '__main__':
     with open('routes.txt') as r:
         routeslines = r.readlines()
-    
+
     routes = []
     for line in routeslines:
         routes.append(line.split())
@@ -30,17 +30,28 @@ if __name__ == '__main__':
             arp[line.split(" ")[0]] = line.split(" ")[1]
 
     while True:
-        inp = raw_input("Enter PDU")   # Get the input
+        inp = raw_input("Enter PDU: \n")   # Get the input
         temp = inp.split(" ")
+        print temp
         if (len(temp) != 7):
             break
         interface = inp[0]
         src = inp[1]
         dest = inp[2]
-        protocol = int(inp[3])
+        protocol = inp[3]
         ttl = int(inp[4])
         srcport = inp[5]
         destport = inp[6]
+        if (ttl < 1):
+            print (src + "->" + dest + " discarded (TTL expired)")
+        print(src)
+        a = root
+        while (a):
+                #
+                #choose a direction
+                #iterate until
+            print "asdf"
+            a = a.left
 
         if inp == "":       # If it is a blank line...
             break           # ...break the loop
